feabfdb11d3fde1c05d658d087678a3ad3a9de6e
Jan Miksovsky <jan@miksovsky.com>
2024-02-16 17:41:44 -0800

    Add files from early experiments

---
+++ ReadMe.md
@@ -0,0 +1,1 @@
+This project explores using the [Origami](https://weborigami.org/language) language to create an ebook in the EPUB format.
---
+++ src/slug.js
@@ -0,0 +1,12 @@
+export default function slug(filename) {
+  let slug = filename.toLowerCase();
+  // Remove some characters
+  slug = slug.replace(/['’]/, "");
+  // Replace runs of other characters with a hyphen
+  slug = slug.replace(/[^\w\.]+/g, "-");
+  // Remove trailing hyphen
+  if (slug.endsWith("-")) {
+    slug = slug.slice(0, -1);
+  }
+  return slug;
+}
